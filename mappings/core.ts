9be8728db8dfcd460e1132b3bafc750b74ef71de
nazariyv <nazariy@inbox.ru>
2021-09-08 11:10:26 +0100

    lending and renting count

--- mappings/core.ts
+++ mappings/core.ts
@@ -6,8 +6,13 @@ import {
   RentClaimed,
   StopLend,
 } from "../generated/Registry/Registry";
-import { Lending, Renting, User } from "../generated/schema";
+import { Lending, Renting, User, LendingRentingCount } from "../generated/schema";
 import { fetchUser } from "./helpers";
+
+let lrc = new LendingRentingCount("lendingRentingCount");
+lrc.lending = BigInt.fromI32(0);
+lrc.renting = BigInt.fromI32(0);
+lrc.save();
 
 export function handleLend(event: Lend): void {
   let lentParams = event.params;
@@ -20,51 +25,70 @@ export function handleLend(event: Lend): void {
   lending.paymentToken = BigInt.fromI32(lentParams.paymentToken);
   lending.rentClaimed = false;
   lending.lendAmount = BigInt.fromI32(lentParams.lendAmount);
+  lending.availableAmount = BigInt.fromI32(lentParams.lendAmount);
   lending.is721 = lentParams.is721;
   let lender = fetchUser(lentParams.lenderAddress);
   lending.userID = lender.id;
+  lrc.lending = lrc.lending.plus(BigInt.fromI32(1));
   lending.save();
   lender.save();
+  lrc.save();
 }
 
 export function handleRent(event: Rent): void {
   let rentedParams = event.params;
   let lendingId = rentedParams.lendingID.toString();
-  let renting = new Renting(lendingId);
+  let rentingId = rentedParams.rentingID.toString();
+  let lending = Lending.load(lendingId);
+  let renting = new Renting(rentingId);
   renting.renterAddress = rentedParams.renterAddress;
   renting.rentDuration = BigInt.fromI32(rentedParams.rentDuration);
   renting.rentedAt = rentedParams.rentedAt;
   renting.expired = false;
   renting.lendingID = lendingId;
+  renting.rentAmount = BigInt.fromI32(rentedParams.rentAmount);
+  lending.availableAmount = lending.availableAmount.minus(renting.rentAmount);
   let renter = fetchUser(rentedParams.renterAddress);
   renting.userID = renter.id;
+  lrc.renting = lrc.renting.plus(BigInt.fromI32(1));
+  lending.save();
   renting.save();
   renter.save();
+  lrc.save();
 }
 
 export function handleStopRent(event: StopRent): void {
   let returnParams = event.params;
   let renting = Renting.load(returnParams.rentingID.toString());
   let lending = Lending.load(renting.lendingID);
+  lending.availableAmount = lending.availableAmount.plus(renting.rentAmount);
   let renter = User.load(renting.renterAddress.toHexString());
   store.remove("Renting", renting.id);
+  lrc.renting = lrc.renting.minus(BigInt.fromI32(1));
   renting.save();
   renter.save();
   lending.save();
+  lrc.save();
 }
 
 export function handleRentClaimed(event: RentClaimed): void {
   let claimParams = event.params;
   let renting = Renting.load(claimParams.rentingID.toString());
   let lending = Lending.load(renting.lendingID);
+  lending.availableAmount = lending.availableAmount.plus(renting.rentAmount);
   renting.expired = true;
   lending.rentClaimed = true;
+  lrc.renting = lrc.renting.minus(BigInt.fromI32(1));
+  lrc.lending = lrc.lending.plus(BigInt.fromI32(1));
   renting.save();
   lending.save();
+  lrc.save();
 }
 
 export function handleStopLend(event: StopLend): void {
   let lendingStopParams = event.params;
   let lending = Lending.load(lendingStopParams.lendingID.toString());
+  lrc.lending = lrc.lending.minus(BigInt.fromI32(1));
   store.remove('Lending', lending.id);
+  lrc.save();
 }
