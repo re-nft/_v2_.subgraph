764f3c2e11d3efe24213eeabacfafc63a78bfa9d
nazariyv <hi@naz.ooo>
2022-09-16 19:57:54 +0100

    bugfix(helpers): fixes user counter

--- mappings/helpers.ts
+++ mappings/helpers.ts
@@ -1,14 +1,25 @@
 import { BigInt } from "@graphprotocol/graph-ts";
-import { User, LendingRentingCount } from "../generated/schema";
+import { User, LendingRentingCount, UserCount } from "../generated/schema";
 
-let userCursor: i32 = 0;
+const fetchUserCount = (): UserCount => {
+  let userCount = UserCount.load('usercount');
+  if (userCount === null) {
+    userCount = new UserCount('usercount');
+    userCount.count = 0;
+    userCount.save();
+  }
+  return <UserCount>userCount;
+}
 
 export const fetchUser = (address: String): User => {
   let user = User.load(<string>address);
   if (user === null) {
+    let userCount = fetchUserCount();
     user = new User(<string>address);
-    user.cursor = userCursor;
-    userCursor = userCursor + 1;
+    userCount.count = userCount.count + 1;
+    user.cursor = userCount.count;
+
+    userCount.save();
     user.save();
   }
   return <User>user;
