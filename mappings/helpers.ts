a9b00408d6752172217a7e3ec0e788427e3f0801
Victor Florea <victorcristianflorea@gmail.com>
2022-09-27 16:22:32 +0200

    Updated core.ts functions, enabled Lrc function

--- mappings/helpers.ts
+++ mappings/helpers.ts
@@ -1,25 +1,25 @@
-//import { Address, BigInt } from "@graphprotocol/graph-ts";
-import { Counter } from "../generated/schema";
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { Counter, User, LendingRentingCount } from "../generated/schema";
 
-// export const fetchUser = (address: Address): User => {
-//   let user = User.load(address.toHexString());
-//   if (user === null) {
-//     user = new User(address.toHexString());
-//     user.save();
-//   }
-//   return <User>user;
-// };
+export const fetchUser = (address: Address): User => {
+  let user = User.load(address.toHexString());
+  if (user === null) {
+    user = new User(address.toHexString());
+    user.save();
+  }
+  return <User>user;
+};
 
-// export const fetchLrc = (): LendingRentingCount => {
-//   let lrc = LendingRentingCount.load('lrc');
-//   if (lrc === null) {
-//     lrc = new LendingRentingCount('lrc');
-//     lrc.lending = BigInt.fromI32(0);
-//     lrc.renting = BigInt.fromI32(0);
-//     lrc.save();
-//   }
-//   return <LendingRentingCount>lrc;
-// }
+export const fetchLrc = (): LendingRentingCount => {
+  let lrc = LendingRentingCount.load('lrc');
+  if (lrc === null) {
+    lrc = new LendingRentingCount('lrc');
+    lrc.lending = BigInt.fromI32(0);
+    lrc.renting = BigInt.fromI32(0);
+    lrc.save();
+  }
+  return <LendingRentingCount>lrc;
+}
 
 export const fetchCounter = (): Counter => {
   let c = Counter.load('counter');
