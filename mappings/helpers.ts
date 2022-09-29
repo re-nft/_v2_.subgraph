7e5a53bd2c59acc388772d49b67a3027da1be248
bautista baiocchi lora <bautistabaiocchilora@gmail.com>
2022-09-29 10:45:25 -0300

    add: counter functionality to handleLent and handleRented

--- mappings/helpers.ts
+++ mappings/helpers.ts
@@ -1,14 +1,6 @@
 import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
 import { User, Nft, Counter } from "../generated/schema";
 
-export const fetchUser = (address: Address): User => {
-  let user = User.load(address.toHexString());
-  if (user === null) {
-    user = new User(address.toHexString());
-    user.save();
-  }
-  return <User>user;
-};
 
 // id is set to nftAddress::tokenId
 export const fetchNft = (id: string): Nft => {
