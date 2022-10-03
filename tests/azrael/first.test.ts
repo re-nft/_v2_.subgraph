0cbd55206ca21af3e6e035832994bfed78ce982a
bautista baiocchi lora <bautistabaiocchilora@gmail.com>
2022-10-03 09:54:09 -0300

    fix: createNewLentEvent

--- tests/azrael/first.test.ts
+++ tests/azrael/first.test.ts
@@ -3,86 +3,58 @@ import {newMockEvent, test, describe, assert} from 'matchstick-as/assembly/index
 import {Address, BigInt, Bytes, ethereum} from '@graphprotocol/graph-ts'
 import {handleLent} from "../../mappings/core";
 
+export {handleLent}
+
 function createNewLentEvent(
-    id: string,
+    id: i32,
     nftAddress: string,
     tokenId: string,
     lenderAddress: string,
-    maxRentDuration: string,
+    maxRentDuration: i32,
     dailyRentPrice: string,
     nftPrice: string,
-    paymentToken: string,
-    lentAmount: string,
-    isERC721: boolean,
-    lentAt: string
+    paymentToken: i32,
+    lentAmount: i32,
+    isERC721: boolean
 ): Lent {
     let mockEvent = newMockEvent();
 
-    let newLentEvent =  new Lent();
-    newLentEvent.address = mockEvent.address;
-    newLentEvent.logIndex = mockEvent.logIndex;
-    newLentEvent.transactionLogIndex = mockEvent.transactionLogIndex;
-    newLentEvent.logType = mockEvent.logType;
-    newLentEvent.block = mockEvent.block;
-    // newLentEvent.transaction = mockEvent.transaction;
+    let newLentEvent =  new Lent(
+        mockEvent.address,
+        mockEvent.logIndex,
+        mockEvent.transactionLogIndex,
+        mockEvent.logType,
+        mockEvent.block,
+        mockEvent.transaction,
+        mockEvent.parameters,
+        mockEvent.receipt
+    );
 
     newLentEvent.parameters = new Array()
 
-    let lendingIdValue = ethereum.Value.fromUnsignedBigInt(BigInt.fromString(id))
+    let lendingIdValue = ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(id))
     let nftAddressValue = ethereum.Value.fromAddress(Address.fromString(nftAddress))
     let tokenIdValue = ethereum.Value.fromUnsignedBigInt(BigInt.fromString(tokenId))
     let lenderAddressValue = ethereum.Value.fromAddress(Address.fromString(lenderAddress))
-    let maxRentDurationValue = ethereum.Value.fromUnsignedBigInt(BigInt.fromString(maxRentDuration))
+    let maxRentDurationValue = ethereum.Value.fromI32(maxRentDuration)
     let dailyRentPriceValue = ethereum.Value.fromBytes(Bytes.fromHexString(dailyRentPrice))
     let nftPriceValue = ethereum.Value.fromBytes(Bytes.fromHexString(nftPrice))
-    let paymentTokenValue = ethereum.Value.fromUnsignedBigInt(BigInt.fromString(paymentToken))
-    let lentAmountValue = ethereum.Value.fromUnsignedBigInt(BigInt.fromString(lentAmount))
+    let paymentTokenValue = ethereum.Value.fromI32(paymentToken)
+    let lentAmountValue = ethereum.Value.fromI32(lentAmount)
     let isERC721Value = ethereum.Value.fromBoolean(isERC721)
-    let lentAtValue = ethereum.Value.fromUnsignedBigInt(BigInt.fromString(lentAt))
+    let lentAtValue = ethereum.Value.fromUnsignedBigInt(mockEvent.block.timestamp)
 
-    let _lendingId = new ethereum.EventParam()
-    _lendingId.name = "id";
-    _lendingId.value = lendingIdValue;
-
-    let _nftAddress = new ethereum.EventParam();
-    _nftAddress.name = "nftAddress";
-    _nftAddress.value = nftAddressValue;
-
-    let _tokenId = new ethereum.EventParam()
-    _tokenId.name = "tokenId";
-    _tokenId.value = tokenIdValue;
-
-    let _lenderAddress = new ethereum.EventParam()
-    _lenderAddress.name = "lenderAddress";
-    _lenderAddress.value = lenderAddressValue;
-    
-    let _maxRentDuration = new ethereum.EventParam()
-    _maxRentDuration.name = "maxRentDuration";
-    _maxRentDuration.value = maxRentDurationValue;
-
-    let _dailyRentPrice = new ethereum.EventParam()
-    _dailyRentPrice.name = "dailyRentPrice";
-    _dailyRentPrice.value = dailyRentPriceValue;
-
-    let _nftPrice = new ethereum.EventParam()
-    _nftPrice.name = "nftPrice";
-    _nftPrice.value = nftPriceValue;
-
-    let _paymentToken = new ethereum.EventParam()
-    _paymentToken.name = "paymentToken";
-    _paymentToken.value = paymentTokenValue;
-
-    let _lentAmount = new ethereum.EventParam()
-    _lentAmount.name = "lentAmount";
-    _lentAmount.value = lentAmountValue;
-
-    let _isERC721 = new ethereum.EventParam()
-    _isERC721.name = "isERC721";
-    _isERC721.value = isERC721Value;
-
-    let _lentAt = new ethereum.EventParam()
-    _lentAt.name = "lentAt";
-    _lentAt.value = lentAtValue;
+    let _lendingId = new ethereum.EventParam("id", lendingIdValue)
+    let _nftAddress = new ethereum.EventParam("nftAddress", nftAddressValue);
+    let _tokenId = new ethereum.EventParam("tokenId", tokenIdValue);
+    let _lenderAddress = new ethereum.EventParam("lenderAddress", lenderAddressValue);
+    let _maxRentDuration = new ethereum.EventParam("maxRentDuration", maxRentDurationValue);
+    let _dailyRentPrice = new ethereum.EventParam("dailyRentPrice", dailyRentPriceValue);
+    let _nftPrice = new ethereum.EventParam("nftPrice", nftPriceValue);
+    let _paymentToken = new ethereum.EventParam("paymentToken", paymentTokenValue);
+    let _lentAmount = new ethereum.EventParam("lentAmount", lentAmountValue);
+    let _isERC721 = new ethereum.EventParam("isERC721", isERC721Value);
+    let _lentAt = new ethereum.EventParam("lentAt", lentAtValue);
 
     newLentEvent.parameters.push(_lendingId)
     newLentEvent.parameters.push(_nftAddress)
@@ -96,28 +68,26 @@ function createNewLentEvent(
     newLentEvent.parameters.push(_isERC721)
     newLentEvent.parameters.push(_lentAt)
 
-
     return newLentEvent;
 }
 
 describe("handleLent()", () => {
 
     test("creates Lending", () => {
-        // let newLentEvent = createNewLentEvent(
-        //     "1",
-        //     "0x0000000000000000000000000000000000000001",
-        //     "1",
-        //     "0x0000000000000000000000000000000000000002",
-        //     "1",
-        //     "0x0000000000000000000000000000000000000000000000000000000000000001",
-        //     "0x0000000000000000000000000000000000000000000000000000000000000001",
-        //     "1",
-        //     "1",
-        //     true,
-        //     "1"
-        // )
+        let newLentEvent = createNewLentEvent(
+            1,
+            "0x0000000000000000000000000000000000000001",
+            "1",
+            "0x0000000000000000000000000000000000000002",
+            1,
+            "0x0000000000000000000000000000000000000000000000000000000000000001",
+            "0x0000000000000000000000000000000000000000000000000000000000000001",
+            1,
+            1,
+            true
+        )
 
-        // let lending = handleLent(newLentEvent)
+        // handleLent(newLentEvent)
 
 
         assert.assertTrue(true)
