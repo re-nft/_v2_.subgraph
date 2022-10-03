5430ca0d7f52d2367ba99785bcd792ebbfe24778
bautista baiocchi lora <bautistabaiocchilora@gmail.com>
2022-10-03 13:40:23 -0300

    add: Create Multiple Lendings from different users test

--- tests/azrael/lending.test.ts
+++ tests/azrael/lending.test.ts
@@ -92,5 +92,87 @@ describe("handleLent()", () => {
         assertLendingRentingCounterFields(numberOfEvents, 0);
     })
 
+    test("Create Multiple Lendings from different users", () => {
+        let nftAddress = "0x0000000000000000000000000000000000000001"
+
+        let lenderAddress1 = "0x0000000000000000000000000000000000000002"
+        let lenderAddress2 = "0x0000000000000000000000000000000000000003"
+
+        let tokenId = 1;
+        let maxRentDuration = 1;
+        let dailyRentPrice = "0x0000000000000000000000000000000000000000000000000000000000000001";
+        let nftPrice = "0x0000000000000000000000000000000000000000000000000000000000000001";
+        let paymentToken = 1;
+        let lentAmount = 1;
+        let isERC721 = true;
+
+        let newLentEvent1 = createNewLentEvent(
+            '1',
+            nftAddress,
+            tokenId,
+            lenderAddress1,
+            maxRentDuration,
+            dailyRentPrice,
+            nftPrice,
+            paymentToken,
+            lentAmount,
+            isERC721
+        )
+
+        let newLentEvent2 = createNewLentEvent(
+            '2',
+            nftAddress,
+            tokenId,
+            lenderAddress2,
+            maxRentDuration,
+            dailyRentPrice,
+            nftPrice,
+            paymentToken,
+            lentAmount,
+            isERC721
+        )
+
+
+        handleLent(newLentEvent1)
+        handleLent(newLentEvent2)
+
+        assertLendingFields(
+            '1', 
+            nftAddress, 
+            tokenId, 
+            lenderAddress1, 
+            maxRentDuration, 
+            dailyRentPrice, 
+            nftPrice, 
+            paymentToken, 
+            lentAmount, 
+            isERC721, 
+            1,
+            false,
+            newLentEvent1.block.timestamp
+        )
+        assertLendingFields(
+            '2', 
+            nftAddress, 
+            tokenId, 
+            lenderAddress2, 
+            maxRentDuration, 
+            dailyRentPrice, 
+            nftPrice, 
+            paymentToken, 
+            lentAmount, 
+            isERC721, 
+            2,
+            false,
+            newLentEvent1.block.timestamp
+        )
+
+        assertCounterFields(2, 0, 2);
+        assertUserFields(lenderAddress1, 1);
+        assertUserFields(lenderAddress2, 2);
+        assertLendingRentingCounterFields(2, 0);
+        // assertNftFields(nftAddress, tokenId, lentAmount);
+    })
+
 })
 
