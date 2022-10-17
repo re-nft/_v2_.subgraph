2b22069afe1893d4afb44d77a5904202170bdc4f
Victor Florea <victor@renft.io>
2022-10-17 14:30:26 +0200

    add utilities

--- tests/assert-utils.ts
+++ tests/assert-utils.ts
@@ -1,29 +1,66 @@
 import { assert } from "matchstick-as";
-
+import { Renting } from "../generated/schema";
+import { BigInt } from "@graphprotocol/graph-ts";
 export const RENTING_ENTITY = "Renting";
 export const LENDING_ENTITY = "Lending";
 export const COUNTER_ENTITY = "Counter";
 export const USER_ENTITY = "User";
-export const LENDING_RENTING_COUNTER_ENTITY = "LendingRentingCount";
 export const NFT_ENTITY = "Nft";
 
-export function assertCounterFields(
-  lendingCount: i32,
-  rentingCount: i32,
-  userCount: i32
+export function assertLendingFields(
+  isERC721: boolean,
+  lenderAddress: string,
+  nftAddress: string,
+  tokenId: string,
+  lendingId: string,
+  maxRentDuration: i32,
+  dailyRentPrice: string,
+  lendAmount: i32,
+  paymentToken: i32,
+  cursor: i32,
+  lentAt: BigInt,
+  hasRenting: boolean = false
 ): void {
-  assert.fieldEquals(
-    COUNTER_ENTITY,
-    "counter",
-    "lending",
-    lendingCount.toString()
-  );
-  assert.fieldEquals(
-    COUNTER_ENTITY,
-    "counter",
-    "renting",
-    rentingCount.toString()
-  );
+  assert.fieldEquals(LENDING_ENTITY, lendingId, "isERC721", isERC721.toString());
+  assert.fieldEquals(LENDING_ENTITY, lendingId, "lenderAddress", lenderAddress);
+  assert.fieldEquals(LENDING_ENTITY, lendingId, "nftAddress", nftAddress);
+  assert.fieldEquals(LENDING_ENTITY, lendingId, "tokenId", tokenId);
+  assert.fieldEquals(LENDING_ENTITY, lendingId, "maxRentDuration", maxRentDuration.toString());
+  assert.fieldEquals(LENDING_ENTITY, lendingId, "dailyRentPrice", dailyRentPrice);
+  assert.fieldEquals(LENDING_ENTITY, lendingId, "lendAmount", lendAmount);
+  assert.fieldEquals(LENDING_ENTITY, lendingId, "paymentToken", paymentToken);
+  assert.fieldEquals(LENDING_ENTITY, lendingId, "cursor", cursor.toString());
+  assert.fieldEquals(LENDING_ENTITY, lendingId, "lentAt", lentAt.toString());
+  assert.fieldEquals(LENDING_ENTITY, lendingId, "hasRenting", hasRenting.toString());
+
+  let rent = Renting.load(lendingId);
+  if (hasRenting) {
+    assert.assertNotNull(rent);
+  } else {
+    assert.assertNull(rent);
+  }
+}
+
+export function assertRentingFields(
+  renterAddress: string,
+  lendingId: string,
+  rentingId: string,
+  rentAmount: i32,
+  rentDuration: i32,
+  rentedAt: string,
+  cursor: i32
+): void {
+  assert.fieldEquals(RENTING_ENTITY, rentingId, "renterAddress", renterAddress);
+  assert.fieldEquals(RENTING_ENTITY, rentingId, "lendingId", lendingId);
+  assert.fieldEquals(RENTING_ENTITY, rentingId, "rentAmount", rentAmount.toString());
+  assert.fieldEquals(RENTING_ENTITY, rentingId, "rentDuration", rentDuration.toString());
+  assert.fieldEquals(RENTING_ENTITY, rentingId, "rentedAt", rentedAt);
+  assert.fieldEquals(RENTING_ENTITY, rentingId, "cursor", cursor.toString(ßßß));
+}
+
+export function assertCounterFields(lendingCount: i32, rentingCount: i32, userCount: i32): void {
+  assert.fieldEquals(COUNTER_ENTITY, "counter", "lending", lendingCount.toString());
+  assert.fieldEquals(COUNTER_ENTITY, "counter", "renting", rentingCount.toString());
   assert.fieldEquals(COUNTER_ENTITY, "counter", "user", userCount.toString());
 }
 
@@ -32,35 +69,6 @@ export function assertUserFields(address: string, cursor: i32): void {
   assert.fieldEquals(USER_ENTITY, address, "cursor", cursor.toString());
 }
 
-export function assertLendingRentingCounterFields(
-  lendingCount: i32,
-  rentingCount: i32
-): void {
-  assert.fieldEquals(
-    LENDING_RENTING_COUNTER_ENTITY,
-    "lendingRentingCount",
-    "lending",
-    lendingCount.toString()
-  );
-  assert.fieldEquals(
-    LENDING_RENTING_COUNTER_ENTITY,
-    "lendingRentingCount",
-    "renting",
-    rentingCount.toString()
-  );
-  assert.assertTrue(lendingCount >= rentingCount);
-}
-
-// TODO: Nft.id on deployed subgraph does not match the one in unit test. Concerning!
-export function assertNftFields(
-  nftAddress: string,
-  tokenId: i32,
-  lentAmount: i32
-): void {
-  let id = nftAddress
-    .concat("::")
-    .concat(tokenId.toString())
-    .concat("::")
-    .concat(lentAmount.toString());
-  assert.fieldEquals(NFT_ENTITY, id, "id", id);
+export function assertNftFields(lendingId: string): void {
+  assert.fieldEquals(NFT_ENTITY, lendingId, "id", lendingId);
 }
--- tests/event-utils.ts
+++ tests/event-utils.ts
@@ -1,4 +1,4 @@
-import { Lend, Rent } from "../generated/Sylvester/Sylvester";
+import { Lend, Rent, StopLend } from "../generated/Sylvester/Sylvester";
 import { newMockEvent } from "matchstick-as/assembly/index";
 import { Address, BigInt, Bytes, ethereum } from "@graphprotocol/graph-ts";
 
@@ -14,7 +14,7 @@ export function createMultipleNewLendEvents(
   paymentToken: i32
 ): Array<Lend> {
   let lendEvents = new Array<Lend>();
-  for (let i = 1; i <= numberOfEvents; i++) {
+  for (let i = 0; i < numberOfEvents; i++) {
     let newLendEvent = createNewLendEvent(
       isERC721,
       lenderAddress,
@@ -59,47 +59,25 @@ export function createNewLendEvent(
 
   // Creating ethereum values
   let isERC721Value = ethereum.Value.fromBoolean(isERC721);
-  let lenderAddressValue = ethereum.Value.fromAddress(
-    Address.fromString(lenderAddress)
-  );
-  let nftAddressValue = ethereum.Value.fromAddress(
-    Address.fromString(nftAddress)
-  );
-  let tokenIdValue = ethereum.Value.fromUnsignedBigInt(
-    BigInt.fromString(tokenId)
-  );
-  let lendingIdValue = ethereum.Value.fromUnsignedBigInt(
-    BigInt.fromString(lendingId)
-  );
+  let lenderAddressValue = ethereum.Value.fromAddress(Address.fromString(lenderAddress));
+  let nftAddressValue = ethereum.Value.fromAddress(Address.fromString(nftAddress));
+  let tokenIdValue = ethereum.Value.fromUnsignedBigInt(BigInt.fromString(tokenId));
+  let lendingIdValue = ethereum.Value.fromUnsignedBigInt(BigInt.fromString(lendingId));
   let maxRentDurationValue = ethereum.Value.fromI32(maxRentDuration);
-  let dailyRentPriceValue = ethereum.Value.fromBytes(
-    Bytes.fromHexString(dailyRentPrice)
-  );
+  let dailyRentPriceValue = ethereum.Value.fromBytes(Bytes.fromHexString(dailyRentPrice));
   let lendAmountValue = ethereum.Value.fromI32(lendAmount);
   let paymentTokenValue = ethereum.Value.fromI32(paymentToken);
 
   // Configure the parameters
   let _isERC721 = new ethereum.EventParam("isERC721", isERC721Value);
-  let _lenderAddress = new ethereum.EventParam(
-    "lenderAddress",
-    lenderAddressValue
-  );
+  let _lenderAddress = new ethereum.EventParam("lenderAddress", lenderAddressValue);
   let _nftAddress = new ethereum.EventParam("nftAddress", nftAddressValue);
   let _tokenId = new ethereum.EventParam("tokenId", tokenIdValue);
   let _lendingId = new ethereum.EventParam("id", lendingIdValue);
-  let _maxRentDuration = new ethereum.EventParam(
-    "maxRentDuration",
-    maxRentDurationValue
-  );
-  let _dailyRentPrice = new ethereum.EventParam(
-    "dailyRentPrice",
-    dailyRentPriceValue
-  );
+  let _maxRentDuration = new ethereum.EventParam("maxRentDuration", maxRentDurationValue);
+  let _dailyRentPrice = new ethereum.EventParam("dailyRentPrice", dailyRentPriceValue);
   let _lendAmount = new ethereum.EventParam("lentAmount", lendAmountValue);
-  let _paymentToken = new ethereum.EventParam(
-    "paymentToken",
-    paymentTokenValue
-  );
+  let _paymentToken = new ethereum.EventParam("paymentToken", paymentTokenValue);
   // Add parameters to the Lend_params array
   newLendEvent.parameters.push(_isERC721);
   newLendEvent.parameters.push(_lenderAddress);
@@ -115,6 +93,130 @@ export function createNewLendEvent(
 }
 
 // Rent mock event generators
-export function createNewMultipleRentEvents(): Array<Rent> {}
+export function createNewMultipleRentEvents(
+  lendings: Array<Lend>,
+  renterAddress: string,
+  rentAmount: i32,
+  rentDuration: i32,
+  rentedAt: BigInt
+): Array<Rent> {
+  let rentEvents: Array<Rent> = new Array<Rent>();
 
-export function createNewRentEvent(): Rent {}
+  for (let i = 0; i < lendings.length; i++) {
+    let newRentEvent = createNewRentEvent(
+      renterAddress,
+      lendings[i].params.lendingID.toString(),
+      i.toString(),
+      rentAmount,
+      rentDuration,
+      lendings[i].block.timestamp.plus(BigInt.fromI32(rentedAt)).toString()
+    );
+
+    rentEvents.push(newRentEvent);
+  }
+
+  return rentEvents;
+}
+
+export function createNewRentEvent(
+  renterAddress: string,
+  lendingId: string,
+  rentingId: string,
+  rentAmount: i32,
+  rentDuration: i32,
+  rentedAt: string
+): Rent {
+  let mockEvent = newMockEvent();
+
+  let newRentEvent = new Rent(
+    mockEvent.address,
+    mockEvent.transactionLogIndex,
+    mockEvent.logIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    mockEvent.parameters,
+    mockEvent.receipt
+  );
+
+  newRentEvent.parameters = new Array();
+
+  let renterAddressValue = ethereum.Value.fromAddress(Address.fromString(renterAddress));
+  let lendingIdValue = ethereum.Value.fromUnsignedBigInt(BigInt.fromString(lendingId));
+  let rentingIdValue = ethereum.Value.fromUnsignedBigInt(BigInt.fromString(rentingId));
+  let rentAmountValue = ethereum.Value.fromI32(rentAmount);
+  let rentDurationValue = ethereum.Value.fromI32(rentDuration);
+  let rentedAtValue = ethereum.Value.fromUnsignedBigInt(BigInt.fromString(rentedAt));
+
+  let _renterAddress = new ethereum.EventParam("renterAddress", renterAddressValue);
+  let _lendingId = new ethereum.EventParam("lendingId", lendingIdValue);
+  let _rentingId = new ethereum.EventParam("rentingId", rentingIdValue);
+  let _rentAmount = new ethereum.EventParam("rentAmount", rentAmountValue);
+  let _rentDuration = new ethereum.EventParam("rentDuration", rentDurationValue);
+  let _rentedAt = new ethereum.EventParam("rentedAt", rentedAtValue);
+
+  newRentEvent.parameters.push(_renterAddress);
+  newRentEvent.parameters.push(_lendingId);
+  newRentEvent.parameters.push(_rentingId);
+  newRentEvent.parameters.push(_rentAmount);
+  newRentEvent.parameters.push(_rentDuration);
+  newRentEvent.parameters.push(_rentedAt);
+
+  return newRentEvent;
+}
+
+export function createNewStopLendEvent(lendingId: string, stoppedAt: string) {
+  let mockEvent = newMockEvent();
+
+  let newStopLendEvent = new StopLend(
+    mockEvent.address,
+    mockEvent.transactionLogIndex,
+    mockEvent.logIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    mockEvent.parameters,
+    mockEvent.receipt
+  );
+
+  newStopLendEvent.parameters = new Array();
+
+  let lendingIdValue = ethereum.Value.fromUnsignedBigInt(BigInt.fromString(lendingId));
+  let stoppedAtValue = ethereum.Value.fromUnsignedBigInt(BigInt.fromString(stoppedAt));
+
+  let _lendingId = new ethereum.EventParam("lendingId", lendingIdValue);
+  let _stoppedAt = new ethereum.EventParam("stoppedAt", stoppedAtValue);
+
+  newStopLendEvent.parameters.push(_lendingId);
+  newStopLendEvent.parameters.push(_stoppedAt);
+
+  return newStopLendEvent;
+}
+
+export function createNewStopRentEvent(rentingId: string, stoppedAt: string) {
+  let mockEvent = newMockEvent();
+
+  let newStopRentEvent = new StopLend(
+    mockEvent.address,
+    mockEvent.transactionLogIndex,
+    mockEvent.logIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    mockEvent.parameters,
+    mockEvent.receipt
+  );
+
+  newStopRentEvent.parameters = new Array();
+
+  let rentingIdValue = ethereum.Value.fromUnsignedBigInt(BigInt.fromString(rentingId));
+  let stoppedAtValue = ethereum.Value.fromUnsignedBigInt(BigInt.fromString(stoppedAt));
+
+  let _rentingId = new ethereum.EventParam("lendingId", rentingIdValue);
+  let _stoppedAt = new ethereum.EventParam("stoppedAt", stoppedAtValue);
+
+  newStopRentEvent.parameters.push(_rentingId);
+  newStopRentEvent.parameters.push(_stoppedAt);
+
+  return newStopRentEvent;
+}
