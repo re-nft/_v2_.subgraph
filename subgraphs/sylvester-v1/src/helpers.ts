a6f682acc788e39192c1d9edd781a53af4bea6c3
Nazariy Vavryk <hi@naz.ooo>
2023-05-12 22:02:43 +0100

    feat: added sylvester-v1

---
+++ subgraphs/sylvester-v1/src/helpers.ts
@@ -0,0 +1,25 @@
+import { Address } from "@graphprotocol/graph-ts";
+import { User } from "../generated/schema";
+import { Counter } from "../generated/schema";
+
+export const fetchUser = (address: Address): User => {
+  let user = User.load(address.toHexString());
+  if (user === null) {
+    user = new User(address.toHexString());
+    user.save();
+  }
+  return <User>user;
+};
+
+export const fetchCounter = (): Counter => {
+  let c = Counter.load("counter");
+  if (c === null) {
+    c = new Counter("counter");
+    c.lending = 0;
+    c.renting = 0;
+    c.user = 0;
+
+    c.save();
+  }
+  return <Counter>c;
+};
